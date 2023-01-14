862af043fa71967e067e37189d2443678061f345
araa <arguello.ramosadrian@gmail.com>
2023-01-14 14:19:07 -0800

    another domain

--- backend/src/main/java/com/araa/project/Helper/CookieHelper.java
+++ backend/src/main/java/com/araa/project/Helper/CookieHelper.java
@@ -16,14 +16,14 @@ public class CookieHelper {
         String cookie = user.getEmail()+"%"+accessToken+"&%"+refreshToken;
         Cookie setCookie = new Cookie("userLogin",cookie);
         setCookie.setPath("/");
-        setCookie.setDomain("arguelloar.onrender.com");
+        setCookie.setDomain("onrender.com");
         return setCookie;
     }
 
     public static Cookie deleteCookie(){
         Cookie setCookie = new Cookie("userLogin",null);
         setCookie.setPath("/");
-        setCookie.setPath("arguelloar.onrender.com");
+        setCookie.setPath("onrender.com");
         setCookie.setMaxAge(0);
         return setCookie;
     }
