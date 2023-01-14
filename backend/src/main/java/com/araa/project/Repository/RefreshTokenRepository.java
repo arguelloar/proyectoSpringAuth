d9a3d46e2cc086e321774dd42354bdfbd4d439fa
araa <arguello.ramosadrian@gmail.com>
2023-01-14 01:12:03 -0800

    using native queries in jpa

--- backend/src/main/java/com/araa/project/Repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/araa/project/Repository/RefreshTokenRepository.java
@@ -13,10 +13,10 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
     @Transactional
     @Modifying
-    @Query("DELETE FROM RefreshToken r WHERE r.owner_id= ?1")
+    @Query(nativeQuery = true, value = "DELETE FROM proyectopersonal.refresh_token WHERE owner_id= ?1")
     void deleteById(Long id);
 
 
-    @Query("SELECT u FROM RefreshToken u WHERE u.owner_id= ?1")
+    @Query(nativeQuery = true, value = "SELECT * FROM proyectopersonal.refresh_token WHERE owner_id= ?1")
     Optional<RefreshToken> findById(Long aLong);
 }
