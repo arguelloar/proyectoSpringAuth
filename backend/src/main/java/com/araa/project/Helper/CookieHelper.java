1e1e781ac8e75b873d5598866a82ff39d51c98c5
araa <arguello.ramosadrian@gmail.com>
2023-01-14 14:37:39 -0800

    domain fix

--- backend/src/main/java/com/araa/project/Helper/CookieHelper.java
+++ backend/src/main/java/com/araa/project/Helper/CookieHelper.java
@@ -16,14 +16,14 @@ public class CookieHelper {
         String cookie = user.getEmail()+"%"+accessToken+"&%"+refreshToken;
         Cookie setCookie = new Cookie("userLogin",cookie);
         setCookie.setPath("/");
-        setCookie.setDomain("https://arguelloar.onrender.com");
+        setCookie.setDomain("arguelloar.onrender.com");
         return setCookie;
     }
 
     public static Cookie deleteCookie(){
         Cookie setCookie = new Cookie("userLogin",null);
         setCookie.setPath("/");
-        setCookie.setDomain("https://arguelloar.onrender.com");
+        setCookie.setDomain("arguelloar.onrender.com");
         setCookie.setMaxAge(0);
         return setCookie;
     }
